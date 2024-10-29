2102b4b6903491c33988ad20573358f39ca9c1f0
c-fuentes <97372834+c-fuentes@users.noreply.github.com>
2024-10-28 21:58:45 -0400

    Add files via upload

--- src/com/shashi/service/impl/CouponServiceImpl.java
+++ src/com/shashi/service/impl/CouponServiceImpl.java
@@ -13,5 +13,105 @@ import com.shashi.service.CouponService;
 import com.shashi.utility.DBUtil;
 
 public class CouponServiceImpl implements CouponService{
+	@Override
+	public boolean validCoupon(String code) {
+		Connection con = DBUtil.provideConnection();
+
+		PreparedStatement ps = null;
+
+		ResultSet rs = null;
+		
+		boolean result = true;
+
+		try {
+			ps = con.prepareStatement("select * from coupon where code=?");
+
+			ps.setString(1, code);
+
+			rs = ps.executeQuery();
+
+			if (rs.next() && !rs.wasNull()) {
+				result = true;
+				System.out.println(result);
+			}else {
+				result = false;
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		DBUtil.closeConnection(con);
+		DBUtil.closeConnection(ps);
+		DBUtil.closeConnection(rs);
+		
+		System.out.println(result);
+		
+		return result;
+		
+	}
 	
+	@Override
+	public double getCouponDiscount(String code) {
+		Connection con = DBUtil.provideConnection();
+
+		PreparedStatement ps = null;
+
+		ResultSet rs = null;
+		
+		double discount = 0.0;
+		
+		try {
+			ps = con.prepareStatement("select discount from coupon where code=?");
+
+			ps.setString(1, code);
+
+			rs = ps.executeQuery();
+
+			if (rs.next() && !rs.wasNull()) {
+				discount = rs.getDouble(1);
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		DBUtil.closeConnection(con);
+		DBUtil.closeConnection(ps);
+		DBUtil.closeConnection(rs);
+		
+		return discount;
+	}
+	
+	@Override
+	public int getCouponMaxApplicableQuantity(String code) {
+		Connection con = DBUtil.provideConnection();
+
+		PreparedStatement ps = null;
+
+		ResultSet rs = null;
+		
+		int maxQuantity = 999999999;
+		
+		try {
+			ps = con.prepareStatement("select max_Quantity from coupon where code=?");
+
+			ps.setString(1, code);
+
+			rs = ps.executeQuery();
+
+			if (rs.next() && !rs.wasNull()) {
+				maxQuantity = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		DBUtil.closeConnection(con);
+		DBUtil.closeConnection(ps);
+		DBUtil.closeConnection(rs);
+		
+		return maxQuantity;
+	}
 }
