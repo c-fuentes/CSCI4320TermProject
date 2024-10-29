de56603f24332752cf92516dbd5d77c26c9c9245
c-fuentes <97372834+c-fuentes@users.noreply.github.com>
2024-10-28 21:57:27 -0400

    Add files via upload

--- src/com/shashi/service/CouponService.java
+++ src/com/shashi/service/CouponService.java
@@ -6,5 +6,9 @@ import com.shashi.beans.CouponBean;
 
 public interface CouponService{
 	
+	public boolean validCoupon(String code);
 	
+	public double getCouponDiscount(String code);
+	
+	public int getCouponMaxApplicableQuantity(String code);
 }
